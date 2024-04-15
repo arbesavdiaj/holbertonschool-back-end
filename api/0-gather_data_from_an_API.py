18648e04d7b2b866190d68e8b83f3d6ed21d7008
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-04-15 12:51:08 +0200

    add new code

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -18,13 +18,13 @@ if __name__ == "__main__":
     user_name = data['name']
 
     data = requests.get(f"{URL}todos")
+    
     all_todos = data.json()
     user_todos = [todo for todo in all_todos if todo['userId'] == int(argv[1])]
     nr_tasks = len(user_todos)
     comp_tasks = [comp for comp in user_todos if comp['completed'] is True]
     comp_title = [title['title'] for title in comp_tasks]
 
-    print(f"Employee {user_name} is done", end=" ")
-    print(f" with tasks({len(comp_tasks)}/{nr_tasks}):")
+    print(f"Employee {user_name} is done with tasks({len(comp_tasks)}/{nr_tasks}):")
     for title in comp_title:
-        print(f"\t {title}")
+        print("\t" + "S".join(title.split()))
